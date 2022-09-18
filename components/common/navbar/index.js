b298e10625b8ac8ff3a9c89adf5e609471ea1e07
Mauricio <mauricio.tp_@outlook.com>
2022-09-18 19:35:38 -0300

    generating course page statically

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <section>
@@ -5,38 +7,33 @@ export default function Navbar() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Product
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Features
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Marketplace
-              </a>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+              </Link>
+              <Link href="/marketplace">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Marketplace
+                </a>
+              </Link>
+              <Link href="/blogs">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blogs
+                </a>
+              </Link>
             </div>
             <div>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Company
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-indigo-600 hover:text-indigo-500"
-              >
-                Log in
-              </a>
+              <Link href="/wishlist">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
+              </Link>
+              <Link href="/login">
+                <a className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">
+                  Log in
+                </a>
+              </Link>
             </div>
           </div>
         </nav>
