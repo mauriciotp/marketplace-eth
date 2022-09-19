10ab36297842fc7ae2cf8de30f52d7abad533390
Mauricio <mauricio.tp_@outlook.com>
2022-09-19 15:31:24 -0300

    loading provider

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -1,12 +1,43 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 const Web3Context = createContext({});
 
 export default function Web3Provider({ children }) {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        const web3 = new Web3(provider);
+        setWeb3Api({
+          provider,
+          web3,
+          contract: null,
+          isLoading: false,
+        });
+      } else {
+        setWeb3Api((api) => ({
+          ...api,
+          isLoading: false,
+        }));
+        console.error("Please, install Metamask.");
+      }
+    };
+
+    loadProvider();
+  }, []);
+
   return (
-    <Web3Context.Provider value={{ test: "Hello" }}>
-      {children}
-    </Web3Context.Provider>
+    <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
 }
 
