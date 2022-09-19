34d0daf86bb674bec2daf3b835081eb91f710f90
Mauricio <mauricio.tp_@outlook.com>
2022-09-19 17:32:54 -0300

    checking if web3 is loaded

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,7 +2,7 @@ import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 
 export default function Navbar() {
-  const { connect } = useWeb3();
+  const { connect, isWeb3Loaded } = useWeb3();
 
   return (
     <section>
@@ -32,12 +32,21 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              <span
-                onClick={connect}
-                className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Connect Wallet
-              </span>
+              {isWeb3Loaded ? (
+                <span
+                  onClick={connect}
+                  className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Connect Wallet
+                </span>
+              ) : (
+                <span
+                  onClick={connect}
+                  className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Install Metamask
+                </span>
+              )}
             </div>
           </div>
         </nav>
