1773513b11d70e37b6fa9ddc653e76dd75b2ec0b
Mauricio <mauricio.tp_@outlook.com>
2022-09-19 20:17:40 -0300

    improving ux

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,14 +1,15 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
+import { Button } from "@components/ui/common";
 
 export default function Navbar() {
-  const { connect, isWeb3Loaded } = useWeb3();
+  const { connect, isLoading, isWeb3Loaded } = useWeb3();
 
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -32,20 +33,20 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              {isWeb3Loaded ? (
-                <span
-                  onClick={connect}
-                  className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Connect Wallet
-                </span>
+              {isLoading ? (
+                <Button disabled={true} onClick={connect}>
+                  Loading...
+                </Button>
+              ) : isWeb3Loaded ? (
+                <Button onClick={connect}>Connect</Button>
               ) : (
-                <span
-                  onClick={connect}
-                  className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                <Button
+                  onClick={() =>
+                    window.open("https://metamask.io/download", "_blank")
+                  }
                 >
                   Install Metamask
-                </span>
+                </Button>
               )}
             </div>
           </div>
