6964a1ac680ce89ecab98d8d84cc4c6d121c33c1
Mauricio <mauricio.tp_@outlook.com>
2022-09-22 00:21:25 -0300

    checking for admin account

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import useSWR from "swr";
 
+const adminAddresses = {
+  "0xCf33334DF19218f76b66e6B1Cd4FDeF3496303c1": true,
+};
+
 export const handler = (web3, provider) => () => {
-  const { mutate, ...rest } = useSWR(
+  const { data, mutate, ...rest } = useSWR(
     () => (web3 ? "web3/accounts" : null),
     async () => {
       const accounts = await web3.eth.getAccounts();
@@ -19,6 +23,8 @@ export const handler = (web3, provider) => () => {
 
   return {
     account: {
+      data,
+      isAdmin: (data && adminAddresses[data]) ?? false,
       mutate,
       ...rest,
     },
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -43,7 +43,7 @@ export default function Navbar() {
               ) : isWeb3Loaded ? (
                 account.data ? (
                   <Button hoverable={false} className="cursor-default">
-                    Hi there
+                    Hi there {account.isAdmin && "admin"}
                   </Button>
                 ) : (
                   <Button onClick={connect}>Connect</Button>
