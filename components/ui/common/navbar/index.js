2bef09436fcf0ff447aa56dbea1bc2fc38e8fac5
Mauricio <mauricio.tp_@outlook.com>
2022-09-23 20:04:39 -0300

    checking if it is necessary to install metamask in a better way

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -5,7 +5,7 @@ import { useAccount } from "@components/hooks/web3";
 import { useRouter } from "next/router";
 
 export default function Navbar() {
-  const { connect, isLoading, isWeb3Loaded } = useWeb3();
+  const { connect, isLoading, requireInstall } = useWeb3();
   const { account } = useAccount();
   const { pathname } = useRouter();
 
@@ -41,15 +41,11 @@ export default function Navbar() {
                 <Button disabled={true} onClick={connect}>
                   Loading...
                 </Button>
-              ) : isWeb3Loaded ? (
-                account.data ? (
-                  <Button hoverable={false} className="cursor-default">
-                    Hi there {account.isAdmin && "admin"}
-                  </Button>
-                ) : (
-                  <Button onClick={connect}>Connect</Button>
-                )
-              ) : (
+              ) : account.data ? (
+                <Button hoverable={false} className="cursor-default">
+                  Hi there {account.isAdmin && "admin"}
+                </Button>
+              ) : requireInstall ? (
                 <Button
                   onClick={() =>
                     window.open("https://metamask.io/download", "_blank")
@@ -57,6 +53,8 @@ export default function Navbar() {
                 >
                   Install Metamask
                 </Button>
+              ) : (
+                <Button onClick={connect}>Connect</Button>
               )}
             </div>
           </div>
