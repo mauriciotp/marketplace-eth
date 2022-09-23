05b30629b5e9dfd82447f70dff5ba74635cd8be8
Mauricio <mauricio.tp_@outlook.com>
2022-09-23 19:15:59 -0300

    preventing the setup hooks function from being called multiple times

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -11,6 +11,7 @@ export default function Web3Provider({ children }) {
     web3: null,
     contract: null,
     isLoading: true,
+    hooks: setupHooks(),
   });
 
   useEffect(() => {
@@ -24,6 +25,7 @@ export default function Web3Provider({ children }) {
           web3,
           contract: null,
           isLoading: false,
+          hooks: setupHooks(web3, provider),
         });
       } else {
         setWeb3Api((api) => ({
@@ -43,7 +45,6 @@ export default function Web3Provider({ children }) {
     return {
       ...web3Api,
       isWeb3Loaded: web3 != null,
-      getHooks: () => setupHooks(web3, provider),
       connect: provider
         ? async () => {
             try {
@@ -69,6 +70,6 @@ export function useWeb3() {
 }
 
 export function useHooks(cb) {
-  const { getHooks } = useWeb3();
-  return cb(getHooks());
+  const { hooks } = useWeb3();
+  return cb(hooks);
 }
