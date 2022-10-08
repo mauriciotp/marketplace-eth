fb08481d3f673721d6d0a13e03a7986a6aa8aa46
Mauricio <mauricio.tp_@outlook.com>
2022-10-08 19:15:44 -0300

    displaying owned courses

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -15,10 +15,13 @@ export default function OwnedCourses({ courses }) {
         <MarketHeader />
       </div>
       <section className="grid grid-cols-1">
-        <OwnedCourseCard>
-          <Message />
-          <Button>Watch the course</Button>
-        </OwnedCourseCard>
+        {ownedCourses.data &&
+          ownedCourses.data.map((course) => (
+            <OwnedCourseCard key={course.id} course={course}>
+              {/* <Message>My custom message!</Message> */}
+              <Button>Watch the course</Button>
+            </OwnedCourseCard>
+          ))}
       </section>
     </BaseLayout>
   );
