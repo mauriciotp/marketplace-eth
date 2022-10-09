bf34a7e4aead7d0ba6f50a986097921d9137c05d
Mauricio <mauricio.tp_@outlook.com>
2022-10-09 16:12:37 -0300

    improving walletbar

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -18,11 +18,11 @@ export default function Card({ course, Footer, disabled }) {
             {course.type}
           </div>
           <Link href={`/courses/${course.slug}`}>
-            <a className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <a className="h-12 block mt-1 text-sm sm:text-lg leading-tight font-medium text-black hover:underline">
               {course.title}
             </a>
           </Link>
-          <p className="mt-2 text-gray-500">
+          <p className="mt-2 text-sm sm:text-base text-gray-500">
             {course.description.substring(0, 70)}...
           </p>
           {Footer && <Footer />}
