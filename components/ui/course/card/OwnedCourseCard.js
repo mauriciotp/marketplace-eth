81004e6c43a6c286ba091467e0e16ab7a9965462
Mauricio <mauricio.tp_@outlook.com>
2022-10-09 18:02:30 -0300

    improving card

--- components/ui/course/card/OwnedCourseCard.js
+++ components/ui/course/card/OwnedCourseCard.js
@@ -11,8 +11,8 @@ export default function OwnedCourseCard({ children, course }) {
 
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="flex h-full">
-        <div className="h-full flex-1 relative">
+      <div className="block sm:flex h-full">
+        <div className="h-72 sm:h-full flex-1 relative">
           <Image
             src={course.coverImage}
             className="object-cover"
@@ -37,15 +37,15 @@ export default function OwnedCourseCard({ children, course }) {
 
           <div className="border-t border-gray-200">
             <dl>
-              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <div className="bg-gray-50 px-4 py-5 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">Course ID</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {course.ownedCourseId}
                 </dd>
               </div>
-              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <div className="bg-white px-4 py-5 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">Proof</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd className="mt-1 text-sm break-words text-gray-900 sm:mt-0 sm:col-span-2">
                   {course.proof}
                 </dd>
               </div>
