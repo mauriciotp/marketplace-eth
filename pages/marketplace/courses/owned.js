8389feaa16cb5bd333fe149e3faf29d0d355834d
Mauricio <mauricio.tp_@outlook.com>
2022-10-10 12:08:55 -0300

    checking for no courses

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -5,6 +5,7 @@ import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function OwnedCourses({ courses }) {
   const router = useRouter();
@@ -15,10 +16,22 @@ export default function OwnedCourses({ courses }) {
     <BaseLayout>
       <MarketHeader />
       <section className="grid grid-cols-1">
+        {ownedCourses.hasInitialResponse &&
+          (!ownedCourses.data || ownedCourses.data.length === 0) && (
+            <div className="w-1/2">
+              <Message type="warning">
+                <div>You don&apos;t own any courses</div>
+                <Link href="/marketplace">
+                  <a className="font-normal hover:underline">
+                    <i>Purchase course</i>
+                  </a>
+                </Link>
+              </Message>
+            </div>
+          )}
         {ownedCourses.data &&
           ownedCourses.data.map((course) => (
             <OwnedCourseCard key={course.id} course={course}>
-              {/* <Message>My custom message!</Message> */}
               <Button onClick={() => router.push(`/courses/${course.slug}`)}>
                 Watch the course
               </Button>
