b3bdf3591cea42670cd73c7e9e71ed7801ae1005
Mauricio <mauricio.tp_@outlook.com>
2022-10-16 23:18:30 -0300

    creating activate course function inside the contract

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -28,6 +28,12 @@ contract CourseMarketplace {
     setContractOwner(msg.sender);
   }
 
+  /// Course has invalid state!
+  error InvalidState();
+
+  /// Course is not created!
+  error CourseIsNotCreated();
+
   /// Course has already a Owner!
   error CourseHasOwner();
 
@@ -59,6 +65,20 @@ contract CourseMarketplace {
     });
   }
 
+  function activateCourse(bytes32 courseHash) external onlyOwner {
+    if (!isCourseCreated(courseHash)) {
+      revert CourseIsNotCreated();
+    }
+
+    Course storage course = ownedCourses[courseHash];
+
+    if (course.state != State.Purchased) {
+      revert InvalidState();
+    }
+
+    course.state = State.Activated;
+  }
+
   function transferOwnership(address newOwner) external onlyOwner {
     setContractOwner(newOwner);
   }
@@ -85,6 +105,12 @@ contract CourseMarketplace {
 
   function setContractOwner(address newOwner) private {
     owner = payable(newOwner);
+  }
+
+  function isCourseCreated(bytes32 courseHash) private view returns (bool) {
+    return
+      ownedCourses[courseHash].owner !=
+      0x0000000000000000000000000000000000000000;
   }
 
   function hasCourseOwnership(bytes32 courseHash) private view returns (bool) {
