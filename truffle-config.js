51b434e0772f080818dd60957f4671109e80a327
Mauricio <mauricio.tp_@outlook.com>
2022-10-23 14:55:13 -0300

    truffle goerli config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
@@ -5,6 +7,21 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: "",
+          },
+          providerOrUrl: "https://goerli.infura.io/v3/YOUR-PROJECT-ID",
+          addressIndex: 0,
+        }),
+      network_id: 5,
+      gas: 5500000, // Gas Limit, how much gas we are willing to spent
+      gasPrice: 20000000000, // How much we are willing to spent fot unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200, // number of blocks before deployment times out
     },
   },
 
